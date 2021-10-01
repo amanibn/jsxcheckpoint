d2e8141972e96e8518f015e564292a2d650b4b02
amanibn <benahmedamani44@gmail.com>
2021-10-01 22:41:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './style.css';
+import myWonderfulImage from "./imageInSrc.jpg"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+        <h1 className="title red">Your name here</h1>
+        <br/>
+        <img width="320" src={myWonderfulImage} />
+        <br/>
+        <img width="320px" src="/imageInPublic.jpg" />
+      </div>
+      <video width="320" height="240" controls>
+      <source src="myVideo.mp4" type="video/mp4" />
+</video>
+     {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +28,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+  </header>*/}
     </div>
   );
 }
